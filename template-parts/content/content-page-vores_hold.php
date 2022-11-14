9d1ff0ee7a9341655df53f1c284b1298fc8c0a76
Tobias Roland <samu1493@stud.kea.dk>
2022-11-14 20:56:20 +0100

    alt data inde

--- template-parts/content/content-page-vores_hold.php
+++ template-parts/content/content-page-vores_hold.php
@@ -38,9 +38,12 @@
 			)
 		);
 		?>
-
-		<button>Fodbold</button>
-		<button>Håndbold</button>
+<nav id="filter_buttons">
+<button data-sport="alle">Alle</button>
+	<button data-sport="3">Fodbold</button>
+	<button data-sport="4">Håndbold</button>
+	</nav>
+		
 		<div id="teamview">
 
 		</div>
@@ -49,40 +52,75 @@
 	<img class="team_pic"src="" alt="">
 	<h3 class="team_name"></h3>
 	<p class="short_description"></p>
+	<p class="kategori"></p>
 	<button class="read_more">Læs mere</button>
 	</div>
 </template>
 	</div><!-- .entry-content -->
 
 	<script>
+
+		// opretter variabler
 		let hold;
+		let categories;
+		let filter_sport= "alle";
 		const team_view = document.querySelector("#teamview");
 		const template =  document.querySelector("template");
 		const url="https://tobiasroland.dk/kea/09_cms/testersite_for_childtheme/wordpress/wp-json/wp/v2/hold?per_page=100";
+		const catUrl="https://tobiasroland.dk/kea/09_cms/testersite_for_childtheme/wordpress/wp-json/wp/v2/categories";
+
+
+		// Henter json med hold og json med categorier
 		async function getJson(){
 			let response = await fetch(url);
+			let catdata = await fetch(catUrl);
 			hold = await response.json();
+			categories = await catdata.json();
+			console.log(categories);
+			addEventListenerToButtons();
 			visHold();
 		}
 
+
+function addEventListenerToButtons(){
+	document.querySelectorAll("#filter_buttons button").forEach(elm => {
+		elm.addEventListener("click", filter);
+	})
+}
+
+function filter(){
+	filter_sport = this.dataset.sport;
+	console.log(filter_sport);
+	visHold();
+
+}
+
+
+
 		function visHold(){
 			console.log(hold);
+
+			team_view.innerHTML ="";
+
 			hold.forEach(team => {
+
+			if (filter_sport == "alle" || team.categories.includes(parseInt(filter_sport))){
+
 				const clone = template.cloneNode(true).content;
 				clone.querySelector("img").src = team.billeder[0].guid;
 				clone.querySelector("h3").textContent = team.title.rendered;
+
 				clone.querySelector(".team").addEventListener("click", () =>{
-					location.href = team.link;
-				})
-
-
+					location.href = team.link;})
 				team_view.appendChild(clone);
 				
-
-			})
-
+			}
+				})
 		}
+
+		
 	getJson();
+	
 	</script>
 
 	<?php if ( get_edit_post_link() ) : ?>
--- template-parts/content/content-single-hold.php
+++ template-parts/content/content-single-hold.php
@@ -17,21 +17,44 @@
 
 
 <h1 class="hold_navn"></h1>
+
+<div class="hold_info">
 <p class="hold_beskrivelse"></p>
 
+<div class="traening_og_tilmeldning">
 <p class="traenings_tider"></p>	
 <p class="traenings_dage"></p>	
 <p class="traenings_lokation"></p>	
 <p class="pris"></p>
 <button>Tilmeld</button>
+</div>
 
-<h3>Trænere</h3>
+</div>
+
+
+<div class="traenere_og_kampklar">
+	<div class="traenere">
+
+	<h3>Trænere</h3>
 <img class="traener_billede" src="" alt="">
 <h3 class="traener_navn"></h3>
 <p class="traener_stilling"></p>
+<p class="traener_tlf"></p>
+<p class="traener_mail"></p>
+	</div>
+	<div class="kampklar">
+		<h2>Her bruger vi kampklar</h2>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ipsum? Architecto adipisci reprehenderit earum. Dignissimos quas dolore delectus quis, fugit molestias laudantium facere nisi vero atque est maxime consectetur laboriosam ea dicta! Nihil temporibus numquam explicabo unde laboriosam veniam quis!</p>
+	</div>
 
+
+</div>
+
+<div class="hold_galleri">
 <h3>Galleri</h3>
 <img class="billedegalleri" src="" alt="">
+
+</div>
 
 
 
@@ -67,6 +90,11 @@
 				document.querySelector(".traener_billede").src = hold.traener_1_billede.guid;
 				document.querySelector(".traener_navn").textContent = hold.traener_1_navn;
 				document.querySelector(".traener_stilling").textContent = hold.traener_1_stilling;
+				document.querySelector(".traener_tlf").textContent = hold.traener_1_telefon;
+				document.querySelector(".traener_mail").textContent = hold.traener_1_mail;
+				document.querySelector(".billedegalleri").src = hold.billeder[0].guid;
+
+		
 				
 
 
