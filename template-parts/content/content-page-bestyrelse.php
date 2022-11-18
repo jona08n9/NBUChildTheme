a50f30171356f25b6e9902fe9b143f092ac2921d
Jachob Westh <jachobwesth@gmail.com>
2022-11-18 13:03:43 +0100

    bestyrrelse

--- template-parts/content/content-page-bestyrelse.php
+++ template-parts/content/content-page-bestyrelse.php
@@ -91,7 +91,7 @@ let medlem;
 				clone.querySelector(".bestyrelsesmedlem_billede").src = medlem.visitkort.guid;
 				clone.querySelector(".medlem_navn").textContent = medlem.title.rendered;
 				clone.querySelector(".medlem_stilling").textContent = medlem.funktion;
-				clone.querySelector(".medlem_mail").innerHTML = `<a href="mailto:${medlem.mail}">${medlem.mail}</a>`;
+				clone.querySelector(".medlem_mail").innerHTML = `<a href="mailto:${medlem.mail}>${medlem.mail}</a>`;
 
 				section_medlem.appendChild(clone);
 
